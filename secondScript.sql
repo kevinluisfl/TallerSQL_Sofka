a24b81ce61a399c7247a9f20f05cdc4b98cd6a92
Kevin Luis FL <ing.kevinflorez@gmail.com>
2022-03-07 20:06:46 -0500

    validando no mostrar informacion de registros borrado lógico

--- secondScript.sql
+++ secondScript.sql
@@ -1,11 +1,19 @@
 -- TODAS LAS CONSULTAS SE REALIZARON EN MARIADB
 -- 1. Obtener productos vendidos por tipo y número de documento
-SELECT c.customer_name AS Customer, c.type_id AS Type_Id , c.number_id AS Number_Id,  p.product_name AS Product
+SELECT c.customer_name AS Customer, c.type_id AS Type_Id , c.number_id AS Number_Id, s.sale_invoice AS Invoice, p.product_name AS Product
 FROM sale_detail sd
 JOIN sale s ON sd.sale_id = s.id_sale
 JOIN customer c ON s.customer_id = c.id_customer
 JOIN product p ON sd.product_id = p.id_product
 WHERE c.type_id = 'CC' AND c.number_id = '136823322';
+
+-- Con esta consulta se omiten las facturas con borradas de forma lógica
+SELECT c.customer_name AS Customer, c.type_id AS Type_Id , c.number_id AS Number_Id, s.sale_invoice AS Invoice, p.product_name AS Product
+FROM sale_detail sd
+JOIN sale s ON sd.sale_id = s.id_sale
+JOIN customer c ON s.customer_id = c.id_customer
+JOIN product p ON sd.product_id = p.id_product
+WHERE c.type_id = 'CE' AND c.number_id = '056345309' AND s.deleted != 'true';
 
 -- 2. Obtener productos por el nombre, mostrar quien o quienes han sido sus proveedores
 SELECT s.supplier_name AS Supplier, p.product_name AS Product, ps.since AS Since
@@ -18,4 +26,12 @@ WHERE p.product_name = 'Manzana';
 SELECT p.product_name AS Product, SUM(sd.quantity) AS Quantity
 FROM sale_detail sd
 JOIN product p ON sd.product_id = p.id_product
+GROUP BY sd.product_id ORDER BY Quantity DESC;
+
+-- Con esta consulta se omiten las facturas con borradas de forma lógica
+SELECT p.product_name AS Product, SUM(sd.quantity) AS Quantity
+FROM sale_detail sd
+JOIN sale s ON sd.sale_id = s.id_sale
+JOIN product p ON sd.product_id = p.id_product
+WHERE s.deleted != 'true'
 GROUP BY sd.product_id ORDER BY Quantity DESC;
